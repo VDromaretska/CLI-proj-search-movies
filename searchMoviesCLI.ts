ba891b41faf5130fc6f9620e686f760febf581da
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-08-14 15:57:44 +0200

    Created async function to connect with specified db and while loop with break statement for prompts from client

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -4,5 +4,20 @@ import { Client } from "pg";
 //As your database is on your local machine, with default port,
 //and default username and password,
 //we only need to specify the (non-default) database name.
-const client = new Client({ database: 'omdb' });
-console.log("Welcome to search-movies-cli!");
+const client = new Client({ database: "omdb" });
+
+async function searchForMovies() {
+  try {
+    await client.connect();
+    console.log("Welcome to search-movies-cli!");
+    while (true) {
+      const input = question("Search for what movie? (or 'q' for quit)  ");
+      if (input.toLowerCase() === "q") {
+        break;
+      }
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+searchForMovies();
