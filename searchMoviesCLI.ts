a641bcb50eb34503d8c416ed583c77a40369dd99
VDromaretska <viktoriya.dromaretska@gmail.com>
2023-08-14 16:59:31 +0200

    CLI query for movie name search

--- searchMoviesCLI.ts
+++ searchMoviesCLI.ts
@@ -9,15 +9,25 @@ const client = new Client({ database: "omdb" });
 async function searchForMovies() {
   try {
     await client.connect();
+    // const test = await client.query("Select * from movies limit 20");
+    // console.table(test.rows);
     console.log("Welcome to search-movies-cli!");
     while (true) {
       const input = question("Search for what movie? (or 'q' for quit)  ");
       if (input.toLowerCase() === "q") {
         break;
       }
+      const searchedValue = [`%${input}%`];
+      const res = await client.query(
+        "SELECT name, date, kind, budget, revenue from movies WHERE name ilike $1",
+        searchedValue
+      );
+      console.table(res.rows);
     }
   } catch (error) {
     console.error(error);
+  } finally {
+    await client.end();
   }
 }
 searchForMovies();
